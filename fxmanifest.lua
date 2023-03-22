8e970218fc28f3fd12b109fd935ca369e11e2f48
LegoScrap-Development <118598009+LegoScrap-Development@users.noreply.github.com>
2023-03-22 01:16:53 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+games {"gta5"}
+
+author 'LS Development (discord.gg/p8CwDUm7NB)'
+description 'A free miranda card script made by LS Development.'
+version '0.0.1'
+
+files {
+    '*.*',
+    '*/*.*'
+}
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'client/*.lua'
+} 
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/reset.css',
+    'html/listener.js'
+}
